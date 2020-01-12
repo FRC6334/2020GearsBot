28734bbd9e7462619f53224182c3f5c9013b6b56
Chrizafier <christopher.a.carrillo@gmail.com>
2020-01-12 16:56:16 -0500

    added color sensor

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -13,9 +13,11 @@ import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.commands.ArcadeDrive;
+import frc.robot.commands.GetColor;
 import frc.robot.subsystems.DriveTrain;
 import frc.robot.subsystems.USBCamera;
 import frc.robot.subsystems.LimeLightVision;
+import frc.robot.subsystems.ColorSensor;
 import frc.robot.commands.GetLimeLightValues;
 import frc.robot.commands.DriveToTarget;
 
@@ -30,6 +32,7 @@ public class RobotContainer {
   private final DriveTrain m_drivetrain = new DriveTrain();
   private final USBCamera m_camera = new USBCamera();
   private final LimeLightVision m_limelight = new LimeLightVision();
+  private final ColorSensor m_color_sensor = new ColorSensor();
   private final Joystick m_joystick0 = new Joystick(0);
   
   /**
@@ -66,7 +69,7 @@ public class RobotContainer {
     m_button3.whileHeld(new DriveToTarget(m_limelight, m_drivetrain));
   
     final JoystickButton m_button11 = new JoystickButton(m_joystick0, 11);
-    m_button11.whenPressed(new DriveToTarget(m_limelight, m_drivetrain));
+    m_button11.whenPressed(new GetColor(m_color_sensor));
   }
 
 

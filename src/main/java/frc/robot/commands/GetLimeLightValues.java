00e62479d3b61f6cc72083938127d54ff6feabbd
Kevin Armstrong <kevinarmstrong@Saras-MacBook-Pro-2.local>
2020-01-12 21:23:00 -0500

    changed get lime light values to an instant command

--- src/main/java/frc/robot/commands/GetLimeLightValues.java
+++ src/main/java/frc/robot/commands/GetLimeLightValues.java
@@ -7,12 +7,11 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.subsystems.LimeLightVision;
 
-public class GetLimeLightValues extends CommandBase {
+public class GetLimeLightValues extends InstantCommand {
   LimeLightVision lime_light;
-  boolean m_finished;
 
   /**
    * Creates a new GetLimeLightValues.
@@ -25,24 +24,6 @@ public class GetLimeLightValues extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    m_finished = false;
-  }
-
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
     lime_light.outputLimeLightValues();
-    m_finished = true;
-  }
-
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {
-  }
-
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return m_finished;
   }
 }
